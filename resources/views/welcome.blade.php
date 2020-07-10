f7980b4de751d0ead8474c6349fea1800a2327d6
choca <aldairchoca@gmail.com>
2020-07-10 15:59:13 -0500

    Se hacen las migraciones con las del paquete de roles y permisos

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Tienda Laravel
+                    Tienda Laravel Development
                 </div>
 
                 <div class="links">
